12db32d1bc95a3351e61a76a49f2a0f76fe6a3de
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-23 21:49:30 +0100

    Define likely() and unlikely() macros

--- webreaper.h
+++ webreaper.h
@@ -1,4 +1,9 @@
 #ifndef WEBREAPER_H
 #define WEBREAPER_H 1
 
+#define clear_struct(s) memset((s), 0, sizeof(*(s)))
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 #endif /* !defined WEBREAPER_H
