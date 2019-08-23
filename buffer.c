c5ada2885dba8c40c26141a10c6f338db0aa48e8
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-23 18:59:55 +0100

    Add buf_integrity() and BUFFER_MAGIC

---
+++ buffer.c
@@ -0,0 +1,140 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "buffer.h"
+
+int
+buf_integrity(buf_t *buf)
+{
+	assert(buf);
+
+	if (buf->magic != BUFFER_MAGIC)
+		return 0,
+	else
+		return 1;
+}
+
+void
+buf_collapse(buf_t *buf, off_t offset, size_t range)
+{
+	if (range > buf->buf_size || offset >= buf->buf_size)
+		return;
+
+	char *to = (buf->data + offset);
+	char *from = (to + range);
+	char *end = buf->buf_end;
+	size_t bytes;
+
+	if (range == buf->buf_size)
+	{
+		buf_clear(buf);
+		return;
+	}
+
+	bytes = (end - from);
+
+	memmove(to, from, bytes);
+	to = (end - range);
+	memset(to, 0, range);
+
+	buf->buf_tail -= range;
+
+	if (buf->buf_tail < buf->buf_head)
+		buf->buf_tail = buf->buf_head;
+
+	return;
+}
+
+int
+buf_extend(buf_t *buf, size_t by)
+{
+	assert(buf);
+	assert(buf->data);
+
+	size_t	new_size = (by + buf->buf_size);
+	size_t	tail_off;
+	size_t	head_off;
+
+	tail_off = buf->buf_tail - buf->data;
+	head_off = buf->buf_head - buf->data;
+
+	if (!(buf->data = realloc(buf->data, new_size)))
+	{
+		fprintf(stderr, "buf_extend: realloc error (%s)\n", strerror(errno));
+		return -1;
+	}
+
+	buf->buf_end = (buf->data + new_size);
+	buf->buf_head = (buf->data + head_off);
+	buf->buf_tail = (buf->data + tail_off);
+	buf->buf_size = new_size;
+
+	return 0;
+}
+
+void
+buf_clear(buf_t *buf)
+{
+	memset(buf->data, 0, buf->buf_size);
+	buf->buf_head = buf->buf_tail = buf->data;
+}
+
+int
+buf_append(buf_t *buf, char *str)
+{
+	size_t len = strlen(str);
+	size_t new_size;
+
+	new_size = (buf->buf_size + len);
+
+	if (new_size > buf_slack(buf))
+	{
+		buf_extend(buf, len);
+	}
+	else
+	{
+		buf->buf_tail += len;
+	}
+
+	strcat(buf->buf_tail, str);
+
+	return 0;
+}
+
+int
+buf_init(buf_t *buf, size_t bufsize)
+{
+	memset(buf, 0, sizeof(*buf));
+
+	if (!(buf->data = calloc(bufsize, 1)))
+	{
+		perror("buf_init: calloc error");
+		return -1;
+	}
+
+	memset(buf->data, 0, bufsize);
+	buf->buf_size = bufsize;
+	buf->buf_end = (buf->data + bufsize);
+	buf->buf_head = buf->buf_tail = buf->data;
+
+	return 0;
+}
+
+void
+buf_destroy(buf_t *buf)
+{
+	assert(buf);
+
+	if (buf->data)
+	{
+		memset(buf->data, 0, buf->buf_size);
+		free(buf->data);
+		buf->data = NULL;
+	}
+
+	memset(buf, 0, sizeof(*buf));
+
+	return;
+}
---
+++ buffer.h
@@ -0,0 +1,29 @@
+#ifndef __BUFFER_H
+#define __BUFFER_H 1
+
+#define DEFAULT_BUFSIZE 16384
+#define BUFFER_MAGIC 0x12344321
+
+typedef struct buf_t
+{
+	char			*data;
+	char			*buf_end; /* End of the allocated buffer memory */
+	char			*buf_head; /* Start of our data */
+	char			*buf_tail; /* End of our data */
+	size_t		buf_size;
+	unsigned	magic = BUFFER_MAGIC;
+} buf_t;
+
+#define buf_used(b) ((b)->buf_tail - (b)->buf_head)
+#define buf_slack(b)	((b)->buf_size - buf_used(b))
+
+int buf_init(buf_t *, size_t) __nonnull((1));
+void buf_destroy(buf_t *) __nonnull((1));
+void buf_collapse(buf_t *, off_t, size_t) __nonnull((1));
+int buf_extend(buf_t *, size_t) __nonnull((1)) __wur;
+int buf_append(buf_t *, char *) __nonnull((1,2)) __wur;
+void buf_clear(buf_t *) __nonnull((1));
+int buf_integrity(buf_t *) __nonnull((1)) __wur;
+int toggle_buffer_size(buf_t *, size_t) __nonnull((1)) __wur;
+
+#endif // __BUFFER_H
