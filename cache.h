ad4f930ceaffadea2259ad256ed663bd6348ede2
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-24 20:53:09 +0100

    Use bitmap in wr_cache_t to track free/used objects

--- cache.h
+++ cache.h
@@ -11,7 +11,7 @@ typedef void (*wr_cache_dtor_t)(void *);
 typedef struct wr_cache_t
 {
 	void *cache;
-	int next_free; /* index of next free object */
+	unsigned char *free_bitmap;
 	int nr_free;
 	int capacity;
 	size_t objsize;
@@ -20,18 +20,9 @@ typedef struct wr_cache_t
 	wr_cache_dtor_t dtor;
 } wr_cache_t;
 
-
 void *wr_cache_create(char *, size_t, int, wr_cache_ctor_t, wr_cache_dtor_t);
 void wr_cache_destroy(wr_cache_t *) __nonnull((1));
 void *wr_cache_alloc(wr_cache_t *) __nonnull((1)) __wur;
 void wr_cache_dealloc(wr_cache_t *, void *) __nonnull((1,2));
-
-#define wr_cache_mark_used(c, s) \
-	off_t off = ((char *)(s) - (char *)(c)->cache) / (c)->objsize;\
-	(c)->cache_slots[off] = 1;
-
-#define wr_cache_mark_unused(c, s) \
-	off_t off = ((char *)(s) - (char *)(c)->cache) (c)->objsize;\
-	(c)->cache_slots[off] = 0;
 
 #endif /* WR_CACHE_H */
