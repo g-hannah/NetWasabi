bd4515fbf6923113b0191ec98f617110372fe56d
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-26 19:19:42 +0100

    Reap links from webpages and add to cache

--- src/http/http_parse_links.c
+++ src/http/http_parse_links.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "cache.h"
@@ -16,8 +17,39 @@ http_parse_links(wr_cache_t *cachep, buf_t *buf)
 
 	savep = p;
 
-	while (p < tail && (p = strstr(savep, "http")))
+	while (p < tail && (p = strstr(savep, "href")))
 	{
-		hl = wr_cache_alloc(cachep);
+		savep = p;
+
+		p = strstr(savep, "http");
+
+		if (!p)
+		{
+			p = savep;
+			continue;
+		}
+
+		savep = p;
+
+		p = memchr(savep, '"', (tail - savep));
+
+		if (!p)
+		{
+			p = savep;
+			continue;
+		}
+
+		if ((p - savep) >= HTTP_MAX_URL)
+			continue;
+
+		if (!(hl = wr_cache_alloc(cachep)))
+			return -1;
+
+		strncpy(hl->url, savep, (p - savep));
+
+		hl->url[p - savep] = 0;
+		hl->time_reaped = time(NULL);
 	}
+
+	return 0;
 }
