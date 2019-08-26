26383b94d7212283d70679f8bbe904b35e22ac6b
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-26 23:41:28 +0100

    Set ->used to 1 of new cache object

--- src/http/http_parse_links.c
+++ src/http/http_parse_links.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "buffer.h"
 #include "cache.h"
 #include "http.h"
 
@@ -11,44 +12,44 @@ http_parse_links(wr_cache_t *cachep, buf_t *buf)
 	assert(buf);
 
 	http_link_t		*hl = NULL;
-	char					*p = buf->head;
+	char					*p = NULL;
 	char					*savep = NULL;
 	char					*tail = buf->buf_tail;
 
-	savep = p;
+	p = savep = buf->data;
 
 	while (p < tail && (p = strstr(savep, "href")))
 	{
+		if (!p)
+			break;
+
 		savep = p;
 
 		p = strstr(savep, "http");
 
 		if (!p)
-		{
-			p = savep;
-			continue;
-		}
+			break;
 
 		savep = p;
 
 		p = memchr(savep, '"', (tail - savep));
 
 		if (!p)
-		{
-			p = savep;
-			continue;
-		}
+			break;
 
-		if ((p - savep) >= HTTP_MAX_URL)
+		if ((p - savep) >= HTTP_URL_MAX)
 			continue;
 
 		if (!(hl = wr_cache_alloc(cachep)))
 			return -1;
 
-		strncpy(hl->url, savep, (p - savep));
+		hl->used = 1;
 
+		strncpy(hl->url, savep, (p - savep));
 		hl->url[p - savep] = 0;
 		hl->time_reaped = time(NULL);
+
+		savep = p;
 	}
 
 	return 0;
