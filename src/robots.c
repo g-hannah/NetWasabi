f3f0dc92bf26cdccfc2a7258078e6cb415ab0701
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-27 20:26:22 +0100

    Use github robots.txt file for testing function

--- src/robots.c
+++ src/robots.c
@@ -1,10 +1,14 @@
 #include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "buffer.h"
 #include "cache.h"
 #include "http.h"
+#include "robots.h"
 #include "webreaper.h"
 
 #define GLOB_CHAR '*'
@@ -12,7 +16,6 @@
 
 extern char **token_blacklist;
 
-int parse_robots(buf_t *) __nonnull((1)) __wur;
 static inline void __blacklist_token(const char *token);
 
 int got_global_rules = 0;
@@ -36,17 +39,14 @@ do {\
 	p = memchr((sp), '\n', ((t) - (sp)));\
 	if (p)\
 	{\
-		++(p);\
 		strncpy(line_buffer, (sp), ((p) - (sp)));\
 		line_buffer[(p) - (sp)] = 0;\
-		++(p);\
+		while ((*p) == 0x0a)\
+			++(p);\
 		(sp) = (p);\
 	}\
 	else\
-	{\
 		(sp) = NULL;\
-		break;\
-	}\
 } while(0)
 
 static inline void __blacklist_token(const char *token)
@@ -67,11 +67,10 @@ do {\
 		if (__p)\
 		{\
 			++__p;\
-			__q = __p;\
-			__p = memchr(__q, '\n', (__e - __q));\
-			strncpy(tmp, __q, (__p - __q));\
-			tmp[__p - __q] = 0;\
+			strncpy(tmp, __p, (__e - __p));\
+			tmp[__e - __p] = 0;\
 			__blacklist_token(tmp);\
+			printf("blacklisted \"%s\"\n", tmp);\
 		}\
 	}\
 } while(0)
@@ -79,15 +78,39 @@ do {\
 int
 parse_robots(buf_t *buf)
 {
-	assert(buf);
+	//assert(buf);
 
 	char *p = buf->buf_head;
 	char *savep = NULL;
 	char *tail = buf->buf_tail;
+	static char *robots_file = "/home/oppa/Projects/WebReaper/robots.txt";
+	buf_t _buf;
 
-	savep = p;
+	struct stat statb;
 
-	FIND_TOKEN("User-Agent", p, savep, tail);
+	clear_struct(&statb);
+
+	lstat(robots_file, &statb);
+
+	buf_init(&_buf, statb.st_size);
+
+	int fd = open(robots_file, O_RDONLY);
+
+	if (fd < 0)
+	{
+		fprintf(stderr, "%s\n", strerror(errno));
+		return -1;
+	}
+
+	buf_read_fd(fd, &_buf, statb.st_size);
+
+	assert(_buf.buf_head != _buf.buf_tail);
+
+	tail = _buf.buf_tail;
+
+	savep = p = _buf.data;
+
+	FIND_TOKEN("User-agent", p, savep, tail);
 
 	if (!p)
 		return -1;
@@ -102,6 +125,9 @@ parse_robots(buf_t *buf)
 	if (*(p+1) == GLOB_CHAR)
 		got_global_rules = 1;
 
+	if (got_global_rules)
+		printf("Got rules for \"User-agent: *\"\n");
+
 	CONSUME_UNTIL('\n', savep, p, tail);
 
 	if (!p)
@@ -110,7 +136,7 @@ parse_robots(buf_t *buf)
 	++p;
 	savep = p;
 
-	while (p)
+	while (p < tail)
 	{
 		GET_LINE(p, savep, tail);
 		ADD_RULE(line_buffer);
