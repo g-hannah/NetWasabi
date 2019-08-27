85e82a7e9fb6bf28300a99788de09aba494f8b33
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-27 21:34:49 +0100

    Define buf_read/write_socket/tls() functions and inline functions

--- src/mm/buffer.c
+++ src/mm/buffer.c
@@ -1,22 +1,49 @@
 #include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <openssl/ssl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include "buffer.h"
 
 static inline void
-__push_buf_tail(buf_t *buf, size_t by)
+__buf_reset_head(buf_t *buf)
+{
+	buf->data_len += (buf->buf_head - buf->data);
+	buf->buf_head = buf->data;
+}
+
+static inline void
+__buf_push_tail(buf_t *buf, size_t by)
+{
+	buf->buf_tail -= by;
+	buf->data_len -= by;
+}
+
+static inline void
+__buf_push_head(buf_t *buf, size_t by)
+{
+	buf->buf_head -= by;
+	buf->data_len += by;
+	if (buf->buf_head < buf->data)
+		buf->buf_head = buf->data;
+}
+
+static inline void
+__buf_pull_tail(buf_t *buf, size_t by)
 {
 	buf->buf_tail += by;
 	buf->data_len += by;
 }
 
 static inline void
-__pull_buf_tail(buf_t *buf, size_t by)
+__buf_pull_head(buf_t *buf, size_t by)
 {
-	buf->buf_tail -= by;
+	buf->buf_head += by;
 	buf->data_len -= by;
 }
 
@@ -188,11 +215,151 @@ buf_read_fd(int fd, buf_t *buf, size_t bytes)
 		toread -= n;
 		total_read += n;
 
-		__push_buf_tail(buf, (size_t)n);
+		__buf_pull_tail(buf, (size_t)n);
 	}
 
 	return total_read;
 
 	fail:
 	return (ssize_t)-1;
+}
+
+ssize_t
+buf_read_socket(int sock, buf_t *buf)
+{
+	assert(buf);
+
+	size_t remaining = buf->buf_size;
+	ssize_t n = 0;
+	ssize_t total = 0;
+
+	buf_clear(buf);
+
+	while ((n = recv(sock, buf->buf_tail, remaining, 0)))
+	{
+		if (n < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			else
+				goto fail;
+		}
+
+		__buf_pull_tail(buf, (size_t)n);
+		remaining -= n;
+
+		if (remaining <= 1024)
+		{
+			remaining += buf->buf_size;
+			buf_extend(buf, buf->buf_size);
+		}
+	}
+
+	return total;
+
+	fail:
+	return -1;
+}
+
+ssize_t
+buf_write_socket(int sock, buf_t *buf)
+{
+	assert(buf);
+
+	size_t towrite = buf->data_len;
+	ssize_t n = 0;
+
+	while (towrite > 0)
+	{
+		n = send(sock, buf->buf_head, towrite, 0);
+		if (n < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			else
+				goto fail;
+		}
+
+		__buf_pull_head(buf, (size_t)n);
+		towrite -= n;
+	}
+
+	fail:
+	fprintf(stderr, "buf_write_socket: %s\n", strerror(errno));
+	return -1;
+}
+
+ssize_t
+buf_write_tls(SSL *ssl, buf_t *buf)
+{
+	assert(buf);
+	assert(ssl);
+
+	size_t towrite = buf->data_len;
+	ssize_t n = 0;
+	ssize_t total = 0;
+	int ssl_error;
+	int write_sock;
+	int time_slept = 0;
+	struct timeval timeout = {0};
+	fd_set wrfds;
+
+	write_sock = SSL_get_wfd(ssl);
+	fcntl(write_sock, F_SETFL, O_NONBLOCK);
+	
+	while (towrite > 0)
+	{
+		n = SSL_write(ssl, buf->buf_head, towrite);
+		if (n <= 5 && n > 0)
+		{
+			break;
+		}
+		else
+		if (n < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			else
+			{
+				ssl_error = SSL_get_error(ssl, n);
+				switch(ssl_error)
+				{
+					case SSL_ERROR_NONE:
+						continue;
+					case SSL_ERROR_WANT_WRITE:
+						FD_ZERO(&wrfds);
+						FD_SET(write_sock, &wrfds);
+						timeout.tv_sec = 1;
+						time_slept = select(write_sock+1, NULL, &wrfds, NULL, &timeout);
+						if (time_slept < 0)
+						{
+							fprintf(stderr, "buf_write_tls: select error (%s)\n", strerror(errno));
+							goto fail;
+						}
+						else
+						if (time_slept == 0)
+						{
+							goto out;
+						}
+						else
+						{
+							continue;
+						}
+				}
+			}
+		}
+
+		__buf_pull_head(buf, (size_t)n);
+		towrite -= n;
+		total += n;
+
+	} /* while (towrite > 0) */
+
+	__buf_reset_head(buf);
+
+	out:
+	return total;
+
+	fail:
+	return -1;
 }
