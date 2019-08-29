60ab34ce146bcaf932945c6b7c2534296069f7a3
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-08-29 19:31:14 +0100

    Declare conn_switch_to_tls()

--- include/connection.h
+++ include/connection.h
@@ -22,5 +22,6 @@ void close_connection(connection_t *) __nonnull((1));
 int conn_using_tls(connection_t *) __nonnull((1)) __wur;
 int conn_socket(connection_t *) __nonnull((1)) __wur;
 SSL *conn_tls(connection_t *) __nonnull((1)) __wur;
+int conn_switch_to_tls(connection_t *) __nonnull((1)) __wur;
 
 #endif /* !defined CONNECTION_H */
