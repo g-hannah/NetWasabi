2a4af05ba1c4ed715cce1b774a999b7bd201136d
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-09-01 12:32:49 +0100

    Re-added declarations after debugging

--- include/connection.h
+++ include/connection.h
@@ -18,11 +18,11 @@ typedef struct connection_t
 
 void conn_init(connection_t *) __nonnull((1));
 void conn_destroy(connection_t *) __nonnull((1));
-int open_connection(connection_t *, int) __nonnull((1)) __wur;
-void close_connection(connection_t *) __nonnull((1));
 int conn_using_tls(connection_t *) __nonnull((1)) __wur;
 int conn_socket(connection_t *) __nonnull((1)) __wur;
 SSL *conn_tls(connection_t *) __nonnull((1)) __wur;
 int conn_switch_to_tls(connection_t *) __nonnull((1)) __wur;
+int open_connection(connection_t *, int) __nonnull((1)) __wur;
+void close_connection(connection_t *) __nonnull((1));
 
 #endif /* !defined CONNECTION_H */
