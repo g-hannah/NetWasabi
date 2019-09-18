fcdda3cc82ed321019b85aceb7e062bfdf6c15aa
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-09-18 22:51:27 +0100

    Move MATRIX_* macros to webreaper.h

--- include/webreaper.h
+++ include/webreaper.h
@@ -17,6 +17,59 @@
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
+#define MATRIX_INIT(PTR, NUM, ALEN, TYPE) \
+do {\
+	int i;\
+	(PTR) = calloc((NUM), sizeof(TYPE *));\
+	for (i = 0; i < (NUM); ++i)\
+		(PTR)[i] = (TYPE *)NULL;\
+	for (i = 0; i < (NUM); ++i)\
+		(PTR)[i] = calloc(ALEN+1, sizeof(TYPE));\
+} while (0)
+
+#define MATRIX_DESTROY(PTR, NUM) \
+do { \
+	int i;\
+	if ((PTR))\
+	{\
+		for (i = 0; i < (NUM); ++i)\
+		{\
+			if ((PTR)[i])\
+				free((PTR)[i]);\
+		}\
+		free((PTR));\
+	}\
+} while (0)
+
+#define MATRIX_CHECK_CAPACITY(PTR, CUR_IDX, NUM, SIZE, TYPE)\
+do {\
+	size_t ____i;\
+	size_t ____old_size;\
+	if ((PTR))\
+	{\
+		if ((CUR_IDX) >= (NUM))\
+		{\
+			____old_size = (NUM);\
+			(NUM) *= 2;\
+			(PTR) = realloc((PTR), ((NUM) * sizeof(TYPE *)));\
+			fprintf(stderr,\
+				"num=%lu\n"\
+				"old_size=%lu\n",\
+				(NUM),\
+				____old_size);\
+			for (____i = ____old_size; ____i < (NUM); ++____i)\
+			{\
+				(PTR)[____i] = NULL;\
+				(PTR)[____i] = calloc((SIZE), sizeof(TYPE));\
+			}\
+		}\
+	}\
+} while (0)
+
+#define DEFAULT_MATRIX_SIZE 256
+
+#define SKIP_CRNL(____p) do { while ((*____p) == 0x0a || (*____p) == 0x0d) { ++(____p); }; } while (0)
+
 #define __noret __attribute__((__noreturn__))
 #define __ctor __attribute__((constructor))
 #define __dtor __attribute__((destructor))
@@ -33,8 +86,6 @@
 })
 #endif
 
-#define SKIP_CRNL(____p) do { while ((*____p) == 0x0a || (*____p) == 0x0d) { ++(____p); }; } while (0)
-
 #define OPT_USE_TLS	0x1
 #define OPT_SHOW_REQ_HEADER 0x2
 #define OPT_SHOW_RES_HEADER 0x4
@@ -45,6 +96,10 @@ extern uint32_t runtime_options;
 #define option_set(o) ((o) & runtime_options)
 #define set_option(o) (runtime_options |= (o))
 #define unset_option(o) (runtime_options &= ~(o))
+
+#define ACTION_ING_STR ">>> "
+#define ACTION_DONE_STR "@@@ "
+#define ATTENTION_STR "!!! "
 
 /*
  * For paths that are forbidden as
--- src/parse_links.c
+++ src/parse_links.c
@@ -7,57 +7,8 @@
 #include "buffer.h"
 #include "cache.h"
 #include "http.h"
+#include "utils_url.h"
 #include "webreaper.h"
-
-#define MATRIX_INIT(PTR, NUM, ALEN, TYPE) \
-do {\
-	int i;\
-	(PTR) = calloc((NUM), sizeof(TYPE *));\
-	for (i = 0; i < (NUM); ++i)\
-		(PTR)[i] = (TYPE *)NULL;\
-	for (i = 0; i < (NUM); ++i)\
-		(PTR)[i] = calloc(ALEN+1, sizeof(TYPE));\
-} while (0)
-
-#define MATRIX_DESTROY(PTR, NUM) \
-do { \
-	int i;\
-	if ((PTR))\
-	{\
-		for (i = 0; i < (NUM); ++i)\
-		{\
-			if ((PTR)[i])\
-				free((PTR)[i]);\
-		}\
-		free((PTR));\
-	}\
-} while (0)
-
-#define MATRIX_CHECK_CAPACITY(PTR, CUR_IDX, NUM, SIZE, TYPE)\
-do {\
-	size_t ____i;\
-	size_t ____old_size;\
-	if ((PTR))\
-	{\
-		if ((CUR_IDX) >= (NUM))\
-		{\
-			____old_size = (NUM);\
-			(NUM) *= 2;\
-			(PTR) = realloc((PTR), ((NUM) * sizeof(TYPE *)));\
-			fprintf(stderr,\
-				"num=%lu\n"\
-				"old_size=%lu\n",\
-				(NUM),\
-				____old_size);\
-			for (____i = ____old_size; ____i < (NUM); ++____i)\
-			{\
-				(PTR)[____i] = NULL;\
-				(PTR)[____i] = calloc((SIZE), sizeof(TYPE));\
-			}\
-		}\
-	}\
-} while (0)
-		
 
 static int
 __remove_dups(char **links, int nr)
@@ -96,6 +47,7 @@ __remove_dups(char **links, int nr)
 
 static char **url_links = NULL;
 
+#if 0
 static char *__ignore_tokens__[] =
 {
 	"favicon.ico",
@@ -107,6 +59,7 @@ static char *__ignore_tokens__[] =
 	//".php",
 	NULL
 };
+#endif
 
 static int
 __local_archive_exists(char *link, char *host)
@@ -115,7 +68,7 @@ __local_archive_exists(char *link, char *host)
 	int path_max = 0;
 	int exists = 0;
 	char *home;
-	static char ptmp[1024];
+	static char tmp_page[1024];
 
 	path_max = pathconf("/", _PC_PATH_MAX);
 	if (path_max == 0)
@@ -128,10 +81,8 @@ __local_archive_exists(char *link, char *host)
 	buf_append(&tmp, home);
 	buf_append(&tmp, "/" WEBREAPER_DIR "/");
 	buf_append(&tmp, host);
-	http_parse_page(link, ptmp);
-	buf_append(&tmp, ptmp);
-
-	//buf_append(&tmp, ".html");
+	http_parse_page(link, tmp_page);
+	buf_append(&tmp, tmp_page);
 
 	exists = access(tmp.buf_head, F_OK);
 
@@ -142,8 +93,6 @@ __local_archive_exists(char *link, char *host)
 	else
 		return 0;
 }
-
-#define DEFAULT_MATRIX_SIZE 256
 
 int
 parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
@@ -157,50 +106,45 @@ parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
 	char					*tail;
 	int						nr = 0;
 	size_t url_len = 0;
-	size_t max_len = (HTTP_URL_MAX - strlen("https://"));
 	size_t cur_size = DEFAULT_MATRIX_SIZE;
+	size_t href_len = strlen("href=\"");
 	int aidx = 0;
 	int i;
-	int ignore = 0;
 	buf_t *buf = &conn->read_buf;
 	buf_t url;
-	buf_t tmp;
-	static char page_tmp[1024];
-	static char host_tmp[1024];
+	buf_t full_url;
+	buf_t path;
+
+	buf_init(&url, HTTP_URL_MAX);
+	buf_init(&full_url, HTTP_URL_MAX);
+	buf_init(&path, pathconf("/", _PC_PATH_MAX));
 
 	tail = buf->buf_tail;
 
-	buf_init(&url, HTTP_URL_MAX);
-	buf_init(&tmp, HTTP_URL_MAX);
-
-	MATRIX_INIT(url_links, cur_size, max_len, char);
+	MATRIX_INIT(url_links, cur_size, HTTP_URL_MAX, char);
 
 	savep = buf->buf_head;
 
 	while (1)
 	{
-		ignore = 0;
 		buf_clear(&url);
-		buf_clear(&tmp);
+		buf_clear(&full_url);
+		buf_clear(&path);
 
 		p = strstr(savep, "href=\"");
 
 		if (!p || p >= tail)
 			break;
 
-		savep = (p += strlen("href=\""));
-
-		p = memchr(savep, '?', (p - savep));
-
-		if (!p)
-			p = memchr(savep, '"', (tail - savep));
+		savep = (p += href_len);
+		p = memchr(savep, '"', (tail - savep));
 
 		if (!p)
 			break;
 
 		url_len = (p - savep);
 
-		if (url_len >= max_len || url_len < 5)
+		if (url_len >= HTTP_URL_MAX || url_len < 5)
 		{
 			savep = ++p;
 			continue;
@@ -208,52 +152,23 @@ parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
 
 		MATRIX_CHECK_CAPACITY(url_links, aidx, cur_size, HTTP_URL_MAX, char);
 
-		assert(url_len < max_len);
+		assert(url_len < HTTP_URL_MAX);
 		assert(aidx < cur_size);
-
-	/*
-	 * Different servers of same site (e.g., community.something... / blog.something)
-	 * are coded in the page starting with 2 slashes. Handle this.
-	 */
-		if (!strncmp("//", savep, 2))
-		{
-			if (!option_set(OPT_ALLOW_XDOMAIN))
-			{
-				savep = ++p;
-				continue;
-			}
-
-			savep += 2;
-			url_len -= 2;
-			p = memchr(savep, '/', url_len);
-			if (!p)
-				continue;
-
-			url_len = (p - savep);
-
-			if (option_set(OPT_USE_TLS))
-				buf_append(&tmp, "https://");
-			else
-				buf_append(&tmp, "http://");
-
-			buf_append_ex(&tmp, savep, url_len);
-		}
-		else
-		{
-			buf_append_ex(&tmp, savep, url_len);
-		}
 
 	/*
 	 * Ignore URLs that refer
 	 * to part of their own page.
 	 */
-		if (memchr(tmp.buf_head, '#', url_len))
+		if (memchr(savep, '#', url_len))
 		{
 			savep = ++p;
 			continue;
 		}
 
-		if (!strncmp(tmp.buf_head, "//", 2))
+		buf_append_ex(&url, savep, url_len);
+		make_full_url(conn, &url, &full_url);
+
+		if (is_xdomain(conn, &full_url))
 		{
 			if (!option_set(OPT_ALLOW_XDOMAIN))
 			{
@@ -262,85 +177,14 @@ parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
 			}
 		}
 
-		if (!strncmp("http", tmp.buf_head, 4))
-		{
-			if (!strncmp("https", tmp.buf_head, 5))
-				buf_append(&url, "https://");
-			else
-				buf_append(&url, "http://");
-
-			http_parse_host(tmp.buf_head, host_tmp);
-
-			if (strcmp(host_tmp, conn->primary_host))
-			{
-				if (!option_set(OPT_ALLOW_XDOMAIN))
-				{
-					savep = ++p;
-					continue;
-				}
-			}
-
-			buf_append(&url, host_tmp);
-
-			http_parse_page(tmp.buf_head, page_tmp);
-
-			buf_append(&url, page_tmp);
-		}
-		else
-		{
-			if (option_set(OPT_USE_TLS))
-				buf_append(&url, "https://");
-			else
-				buf_append(&url, "http://");
-
-			buf_append(&url, conn->host);
-
-		/*
-		 * It's a link relative to the current page.
-		 */
-			if (*savep == '.' || *savep != '/')
-			{
-				if (*savep == '.')
-				{
-					savep += 2;
-					url_len = (p - savep);
-				}
-
-				buf_append(&url, conn->page);
-
-				if (*(url.buf_tail - 1) != '/')
-					buf_append(&url, "/");
-
-				buf_append_ex(&url, savep, url_len);
-			}
-			else
-			{
-				/* If we get here, *savep == '/' */
-				buf_append_ex(&url, savep, url_len);
-			}
-		}
-
 		BUF_NULL_TERMINATE(&url);
 
-		fprintf(stderr,
-			"in parse_links: parsed URL=%s\n",
-			url.buf_head);
+#ifdef DEBUG
+		fprintf(stderr, "in parse_links: url=%s\n", full_url.buf_head);
+#endif
 
-		strncpy(url_links[aidx], url.buf_head, url.data_len);
-		url_links[aidx][url.data_len] = 0;
-
-		for (i = 0; __ignore_tokens__[i] != NULL; ++i)
-		{
-			if (strstr(url_links[aidx], __ignore_tokens__[i]))
-			{
-				savep = ++p;
-				ignore = 1;
-				break;
-			}
-		}
-
-		if (ignore)
-			continue;
+		strncpy(url_links[aidx], full_url.buf_head, full_url.data_len);
+		url_links[aidx][full_url.data_len] = 0;
 
 		if (__local_archive_exists(url_links[aidx], host))
 		{
@@ -354,6 +198,8 @@ parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
 	}
 
 	buf_destroy(&url);
+	buf_destroy(&full_url);
+	buf_destroy(&path);
 
 	int removed;
 
@@ -372,10 +218,10 @@ parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
 		assert(wr_cache_obj_used(cachep, (void *)hl));
 
 		if (!hl)
-			goto out_free_links;
+			goto fail_free_links;
 
 		url_len = strlen(url_links[i]);
-		assert(url_len < max_len);
+		assert(url_len < HTTP_URL_MAX);
 		strncpy(hl->url, url_links[i], url_len);
 		hl->url[url_len] = 0;
 		hl->nr_requests = 0;
@@ -388,7 +234,7 @@ parse_links(wr_cache_t *cachep, connection_t *conn, char *host)
 	return 0;
 
 
-	out_free_links:
+	fail_free_links:
 
 	MATRIX_DESTROY(url_links, cur_size);
 	return -1;
