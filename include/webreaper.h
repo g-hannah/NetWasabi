15b2e18cb8a40bd41da302023df38dfe41be424b
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-09-24 20:59:44 +0100

    Define some colours

--- include/webreaper.h
+++ include/webreaper.h
@@ -11,7 +11,8 @@
 #define COL_RED			"\x1b[38;5;9m"
 #define COL_LIGHTRED "\x1b[38;5;204m"
 #define COL_GREEN		"\x1b[38;5;40m"
-#define COL_DARKGREY "\x1b[38;5;245m"
+#define COL_DARKGREY "\x1b[38;5;239m"
+#define COL_DARKBLUE "\x1b[38;5;20m"
 #define COL_END			"\x1b[m"
 
 #define clear_struct(s) memset((s), 0, sizeof(*(s)))
