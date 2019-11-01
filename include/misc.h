8a58d13aea03149196d12f817dc54eeabc93d403
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-01 20:34:38 +0000

    Declare parse_links in misc.h

--- include/misc.h
+++ include/misc.h
@@ -2,7 +2,7 @@
 #define MISC_H 1
 
 #include "buffer.h"
-#include "connection.h"
+#include "http.h"
 
 void check_cookies(connection_t *) __nonnull((1));
 int connection_closed(connection_t *) __nonnull((1)) __wur;
@@ -10,5 +10,6 @@ void check_host(connection_t *) __nonnull((1));
 int check_local_dirs(connection_t *, buf_t *) __nonnull((1,2)) __wur;
 void replace_with_local_urls(connection_t *, buf_t *) __nonnull((1,2));
 int archive_page(connection_t *) __nonnull((1)) __wur;
+int parse_links(struct http_t *, struct cache_ctx *, struct cache_ctx *) __nonnull((1,2,3)) __wur;
 
 #endif /* !defined MISC_H */
