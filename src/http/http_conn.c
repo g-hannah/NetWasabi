f195b630edadc9323cee7f9d5e01ed9826595e7d
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-01 14:14:12 +0000

    Rewrite functions to use struct http_t instead of old connection_t

--- src/http/http_conn.c
+++ src/http/http_conn.c
@@ -34,7 +34,7 @@ __init_openssl(void)
  * @conn: &connection_t that is initialised in this function
  */
 int
-open_connection(struct http_t *http)
+http_connect(struct http_t *http)
 {
 	assert(conn);
 
@@ -42,8 +42,8 @@ open_connection(struct http_t *http)
 	struct addrinfo *ainf = NULL;
 	struct addrinfo *aip = NULL;
 
-	buf_init(&http->conn.read_buf, HTTP_DEFAULT_READ_BUF_SIZE);
-	buf_init(&http->conn.write_buf, HTTP_DEFAULT_WRITE_BUF_SIZE);
+	//buf_init(&http->conn.read_buf, HTTP_DEFAULT_READ_BUF_SIZE);
+	//buf_init(&http->conn.write_buf, HTTP_DEFAULT_WRITE_BUF_SIZE);
 
 	clear_struct(&sock4);
 
@@ -74,7 +74,7 @@ open_connection(struct http_t *http)
 	else
 		sock4.sin_port = htons(HTTP_PORT_NR);
 
-	if ((__http_socket(http) = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ((http_socket(http) = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		put_error_msg("open_connection: connect error (%s)", strerror(errno));
 		goto fail_release_ainf;
@@ -82,7 +82,7 @@ open_connection(struct http_t *http)
 
 	assert(conn->sock > 2);
 
-	if (connect(conn->sock, (struct sockaddr *)&sock4, (socklen_t)sizeof(sock4)) != 0)
+	if (connect(http_socket(http), (struct sockaddr *)&sock4, (socklen_t)sizeof(sock4)) != 0)
 	{
 		put_error_msg("open_connection: connect error (%s)", strerror(errno));
 		goto fail_release_ainf;
@@ -91,19 +91,19 @@ open_connection(struct http_t *http)
 	if (option_set(OPT_USE_TLS))
 	{
 		__init_openssl();
-		conn->ssl_ctx = SSL_CTX_new(TLSv1_2_client_method());
-		conn->ssl = SSL_new(conn->ssl_ctx);
+		http->conn.ssl_ctx = SSL_CTX_new(TLSv1_2_client_method());
+		http_tls(http) = SSL_new(conn->ssl_ctx);
 
-		SSL_set_fd(conn->ssl, conn->sock); /* Set the socket for reading/writing */
-		SSL_set_connect_state(conn->ssl); /* Set as client */
+		SSL_set_fd(http_tls(http), http_socket(http)); /* Set the socket for reading/writing */
+		SSL_set_connect_state(http_tls(http)); /* Set as client */
 	}
 
-	update_connection_state(conn, FL_CONNECTION_CONNECTED);
+	update_connection_state(http, FL_CONNECTION_CONNECTED);
 	freeaddrinfo(ainf);
 	return 0;
 
 	fail_release_ainf:
-	update_connection_state(conn, FL_CONNECTION_DISCONNECTED);
+	update_connection_state(http, FL_CONNECTION_DISCONNECTED);
 	freeaddrinfo(ainf);
 
 	fail:
@@ -111,49 +111,47 @@ open_connection(struct http_t *http)
 }
 
 void
-close_connection(struct http_t *http)
+http_disconnect(struct http_t *http)
 {
 	assert(conn);
 
-	shutdown(conn->sock, SHUT_RDWR);
-	close(conn->sock);
-	conn->sock = -1;
+	shutdown(http_socket(http), SHUT_RDWR);
+	close(http_socket(http));
+	http_socket(http) = -1;
 
-	update_connection_state(conn, FL_CONNECTION_DISCONNECTED);
+	update_connection_state(http, FL_CONNECTION_DISCONNECTED);
 
 	if (option_set(OPT_USE_TLS))
 	{
-		SSL_CTX_free(conn->ssl_ctx);
-		SSL_free(conn->ssl);
-		conn->ssl_ctx = NULL;
-		conn->ssl = NULL;
+		SSL_CTX_free(http->conn.ssl_ctx);
+		SSL_free(http_tls(http));
+		http->conn.ssl_ctx = NULL;
+		http_tls(http) = NULL;
 	}
 
-	buf_destroy(&conn->read_buf);
-	buf_destroy(&conn->write_buf);
+	//buf_destroy(&conn->read_buf);
+	//buf_destroy(&conn->write_buf);
 
 	return;
 }
 
 int
-reconnect(struct http_t *http)
+http_reconnect(struct http_t *http)
 {
-	assert(conn);
-
 	struct sockaddr_in sock4;
 	struct addrinfo *ainf = NULL;
 	struct addrinfo *aip = NULL;
 
-	shutdown(conn->sock, SHUT_RDWR);
-	close(conn->sock);
-	conn->sock = -1;
+	shutdown(http_socket(http), SHUT_RDWR);
+	close(http_socket(http));
+	http_socket(http) = -1;
 
 	if (option_set(OPT_USE_TLS))
 	{
-		SSL_CTX_free(conn->ssl_ctx);
-		SSL_free(conn->ssl);
-		conn->ssl_ctx = NULL;
-		conn->ssl = NULL;
+		SSL_CTX_free(http->conn.ssl_ctx);
+		SSL_free(http_tls(http));
+		http->conn.ssl_ctx = NULL;
+		http_tls(http) = NULL;
 	}
 
 	clear_struct(&sock4);
@@ -176,24 +174,22 @@ reconnect(struct http_t *http)
 	if (!aip)
 		goto fail;
 
-	sprintf(conn->host_ipv4, "%s", inet_ntoa(sock4.sin_addr));
+	sprintf(http->host_ipv4, "%s", inet_ntoa(sock4.sin_addr));
 
-	update_connection_state(conn, FL_CONNECTION_CONNECTING);
+	update_connection_state(http, FL_CONNECTION_CONNECTING);
 
 	if (option_set(OPT_USE_TLS))
 		sock4.sin_port = htons(HTTPS_PORT_NR);
 	else
 		sock4.sin_port = htons(HTTP_PORT_NR);
 
-	if ((conn->sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ((http_socket(http) = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		put_error_msg("open_connection: connect error (%s)", strerror(errno));
 		goto fail_release_ainf;
 	}
 
-	assert(conn->sock > 2);
-
-	if (connect(conn->sock, (struct sockaddr *)&sock4, (socklen_t)sizeof(sock4)) != 0)
+	if (connect(http_socket(http), (struct sockaddr *)&sock4, (socklen_t)sizeof(sock4)) != 0)
 	{
 		put_error_msg("open_connection: connect error (%s)", strerror(errno));
 		goto fail_release_ainf;
@@ -201,41 +197,41 @@ reconnect(struct http_t *http)
 
 	if (option_set(OPT_USE_TLS))
 	{
-		conn->ssl_ctx = SSL_CTX_new(TLSv1_2_client_method());
-		conn->ssl = SSL_new(conn->ssl_ctx);
+		http->conn.ssl_ctx = SSL_CTX_new(TLSv1_2_client_method());
+		http_tls(http) = SSL_new(conn->ssl_ctx);
 
-		SSL_set_fd(conn->ssl, conn->sock); /* Set the socket for reading/writing */
-		SSL_set_connect_state(conn->ssl); /* Set as client */
+		SSL_set_fd(http_tls(http), http_socket(http)); /* Set the socket for reading/writing */
+		SSL_set_connect_state(http_tls(http)); /* Set as client */
 	}
 
 	SET_SOCK_FLAG_ONCE = 0;
 	SET_SSL_SOCK_FLAG_ONCE = 0;
 
-	update_connection_state(conn, FL_CONNECTION_CONNECTED);
+	update_connection_state(http, FL_CONNECTION_CONNECTED);
 	freeaddrinfo(ainf);
 	return 0;
 
 	fail_release_ainf:
 	freeaddrinfo(ainf);
 
-	update_connection_state(conn, FL_CONNECTION_DISCONNECTED);
+	update_connection_state(http, FL_CONNECTION_DISCONNECTED);
 
 	fail:
 	return -1;
 }
 
 int
-conn_switch_to_tls(struct http_t *http)
+http_switch_tls(struct http_t *http)
 {
-	close_connection(conn);
+	http_disconnect(http);
 
 #ifdef DEBUG
-	update_operation_status("Switching to TLS (%s)", conn->host);
+	update_operation_status("Switching to TLS (%s)", http->host);
 #endif
 
 	set_option(OPT_USE_TLS);
 
-	if (open_connection(conn) < 0)
+	if (http_connect(http) < 0)
 		goto fail;
 
 	return 0;
