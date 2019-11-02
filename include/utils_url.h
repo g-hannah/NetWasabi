00dd19625aa4076c3d8753c19116a9c4d65d9b73
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-02 22:12:17 +0000

    Remove obsolete connection_t usage

--- include/utils_url.h
+++ include/utils_url.h
@@ -2,12 +2,12 @@
 #define UTILS_URL_H 1
 
 #include "buffer.h"
-#include "connection.h"
+#include "http.h"
 
-int make_full_url(connection_t *, buf_t *, buf_t *) __nonnull((1,2,3)) __wur;
-int make_local_url(connection_t *, buf_t *, buf_t *) __nonnull((1,2,3)) __wur;
+int make_full_url(struct http_t *, buf_t *, buf_t *) __nonnull((1,2,3)) __wur;
+int make_local_url(struct http_t *, buf_t *, buf_t *) __nonnull((1,2,3)) __wur;
 void encode_url(buf_t *) __nonnull((1));
-int is_xdomain(connection_t *, buf_t *) __nonnull((1,2)) __wur;
+int is_xdomain(struct http_t *, buf_t *) __nonnull((1,2)) __wur;
 int local_archive_exists(char *) __nonnull((1)) __wur;
 int has_extension(char *) __nonnull((1)) __wur;
 
