198ccfa4af6f37b7540c3e9b3b53e1edb62110b8
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-02 13:27:19 +0000

    Remove http_send_head() and http_send_get()

--- src/request.c
+++ src/request.c
@@ -9,124 +9,6 @@
 #include "webreaper.h"
 
 int
-send_head_request(struct http_t *http)
-{
-	assert(http);
-
-	buf_t *wbuf = &http_wbuf(http);
-	buf_t *rbuf = &http_rbuf(http);
-	//char *tmp_cbuf = NULL;
-	int status_code = 0;
-	int rv;
-
-	buf_clear(wbuf);
-
-	update_operation_status("Sending HEAD request to server");
-
-	check_host(http);
-
-	if (!(tmp_cbuf = wr_calloc(8192, 1)))
-		goto fail_free_bufs;
-
-	http_build_request_header(http, HTTP_HEAD);
-
-#if 0
-	sprintf(tmp_cbuf,
-			"HEAD %s HTTP/%s\r\n"
-			"User-Agent: %s\r\n"
-			"Host: %s\r\n"
-			"Connection: keep-alive%s",
-			http->full_url, HTTP_VERSION,
-			HTTP_USER_AGENT,
-			http->host,
-			HTTP_EOH_SENTINEL);
-
-	buf_append(wbuf, tmp_cbuf);
-
-	//check_cookies(conn);
-#endif
-
-	buf_clear(rbuf);
-
-	//free(tmp_cbuf);
-	//tmp_cbuf = NULL;
-
-	if (http_send_request(http) < 0)
-		goto fail;
-
-	rv = http_recv_response(http);
-
-	if (rv < 0 || FL_OPERATION_TIMEOUT == rv)
-		goto fail;
-
-	status_code = http_status_code_int(rbuf);
-
-	return status_code;
-
-	fail:
-	return rv;
-}
-
-int
-send_get_request(struct http_t *http)
-{
-	assert(http);
-
-	buf_t *wbuf = &http_wbuf(http);
-	buf_t *rbuf = &http_rbuf(http);
-	//char *tmp_cbuf = NULL;
-	int status_code = 0;
-	int rv;
-
-	buf_clear(wbuf);
-
-	update_operation_status("Sending GET request to server");
-
-	check_host(http);
-
-	http_build_request_header(http, HTTP_GET);
-
-#if 0
-	if (!(tmp_cbuf = wr_calloc(8192, 1)))
-		goto fail_free_bufs;
-
-	sprintf(tmp_cbuf,
-			"GET %s HTTP/%s\r\n"
-			"User-Agent: %s\r\n"
-			"Host: %s\r\n"
-			"Connection: keep-alive%s",
-			conn->full_url, HTTP_VERSION,
-			HTTP_USER_AGENT,
-			conn->host,
-			HTTP_EOH_SENTINEL);
-
-	buf_append(wbuf, tmp_cbuf);
-
-	check_cookies(conn);
-#endif
-
-	buf_clear(rbuf);
-
-	//free(tmp_cbuf);
-	//tmp_cbuf = NULL;
-
-	if (http_send_request(http) < 0)
-		goto fail;
-
-	rv = http_recv_response(http);
-
-	if (rv < 0 || FL_OPERATION_TIMEOUT == rv)
-		goto fail;
-
-	status_code = http_status_code_int(rbuf);
-
-	return status_code;
-
-	fail:
-	return rv;
-}
-
-int
 do_request(struct http_t *http)
 {
 	assert(http);
@@ -138,36 +20,26 @@ do_request(struct http_t *http)
 	 * Save bandwidth: send HEAD first.
 	 */
 	resend_head:
-	status_code = send_head_request(http);
+	status_code = http_send_request(http, HTTP_HEAD);
 
 	update_status_code(status_code);
 
-	switch(status_code)
-	{
-/*
- * Check here too because 301 may send different
- * spelling (upper-case vs lower-case... etc)
- */
-			if (local_archive_exists(http->full_url))
-				return HTTP_ALREADY_EXISTS;
-			goto resend_head;
-			break;
-		case HTTP_OK:
-			break;
-		default:
-			return status_code;
-	}
+	if (HTTP_OK != status_code)
+		return status_code;
+
+	if (local_archive_already_exists(http->full_url))
+		return HTTP_ALREADY_EXISTS;
 
 	if (connection_closed(http))
 	{
 		//fprintf(stdout, "%s%sRemote peer closed connection%s\n", COL_RED, ACTION_DONE_STR, COL_END);
 		//__show_response_header(&http_rbuf(http));
-		update_operation_status("Remove peer closed connection");
+		update_operation_status("Remote peer closed connection");
 		http_reconnect(http);
 	}
 
 	status_code &= ~status_code;
-	status_code = send_get_request(http);
+	status_code = http_send_request(http, HTTP_GET);
 
 	update_status_code(status_code);
 
