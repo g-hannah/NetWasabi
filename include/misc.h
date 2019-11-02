43e97ce159f4287ad1a2b4e62e79cbdf90abd1bd
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-02 22:10:41 +0000

    Remove unused or moved function declarations

--- include/misc.h
+++ include/misc.h
@@ -4,12 +4,9 @@
 #include "buffer.h"
 #include "http.h"
 
-void check_cookies(connection_t *) __nonnull((1));
-int connection_closed(connection_t *) __nonnull((1)) __wur;
-void check_host(connection_t *) __nonnull((1));
-int check_local_dirs(connection_t *, buf_t *) __nonnull((1,2)) __wur;
-void replace_with_local_urls(connection_t *, buf_t *) __nonnull((1,2));
-int archive_page(connection_t *) __nonnull((1)) __wur;
+int check_local_dirs(struct http_t *, buf_t *) __nonnull((1,2)) __wur;
+void replace_with_local_urls(struct http_t *, buf_t *) __nonnull((1,2));
+int archive_page(struct http_t *) __nonnull((1)) __wur;
 int parse_links(struct http_t *, struct cache_ctx *, struct cache_ctx *) __nonnull((1,2,3)) __wur;
 
 #endif /* !defined MISC_H */
