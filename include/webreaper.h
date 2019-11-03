7abf298190f513d6d3f1659086ed518e63058b6d
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-03 15:13:26 +0000

    Merge misc.h with webreaper.h

--- include/webreaper.h
+++ include/webreaper.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include "buffer.h"
 #include "cache.h"
 #include "graph.h"
 #include "http.h"
@@ -190,6 +191,11 @@ void update_cache1_count(int);
 void update_cache2_count(int);
 void update_cache_status(int, int);
 void put_error_msg(const char *, ...) __nonnull ((1));
+
+int check_local_dirs(struct http_t *, buf_t *) __nonnull((1,2)) __wur;
+void replace_with_local_urls(struct http_t *, buf_t *) __nonnull((1,2));
+int archive_page(struct http_t *) __nonnull((1)) __wur;
+int parse_links(struct http_t *, struct cache_ctx *, struct cache_ctx *) __nonnull((1,2,3)) __wur;
 
 #define TOKEN_MAX 64
 
