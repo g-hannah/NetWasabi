c508bf8e0e5d7abfa5477241a79932a2d3251701
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-11-04 12:36:16 +0000

    Remove unused struct type and update functiond declaration

--- include/fast_mode.h
+++ include/fast_mode.h
@@ -6,13 +6,6 @@
 
 #define FAST_MODE_NR_WORKERS 4
 
-struct worker_ctx
-{
-	wr_cache_t *cache1; /* shared with other workers */
-	wr_cache_t *cache2; /* shared with other workers */
-	struct http_t *http; /* private */
-};
-
-int do_fast_mode(const char *) __nonnull((1)) __wur;
+int do_fast_mode(char *) __nonnull((1)) __wur;
 
 #endif /* !defined FAST_MODE_H */
