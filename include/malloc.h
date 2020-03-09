4a5162f1d5cec1de725c0b05ceac4df0f020637b
gamh86 <g.amh.86@outlook.fr>
2020-03-09 23:26:38 +0000

    Change func names

--- include/malloc.h
+++ include/malloc.h
@@ -3,10 +3,10 @@
 
 #include <stdlib.h>
 
-void *wr_malloc(size_t) __wur;
-void *wr_zmalloc(size_t) __wur;
-void *wr_calloc(int, size_t) __wur;
-void *wr_realloc(void *, size_t) __nonnull((1)) __wur;
-char *wr_strdup(const char *) __nonnull((1)) __wur;
+void *nw_malloc(size_t) __wur;
+void *nw_zmalloc(size_t) __wur;
+void *nw_calloc(int, size_t) __wur;
+void *nw_realloc(void *, size_t) __nonnull((1)) __wur;
+char *nw_strdup(const char *) __nonnull((1)) __wur;
 
 #endif /* !defined MALLOC_H */
