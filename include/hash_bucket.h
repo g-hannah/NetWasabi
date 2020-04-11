330725775ca1e324dcf3f9cfc6b803a01521aabf
gamh86 <g.amh.86@outlook.fr>
2020-04-11 18:14:44 +0100

    Readjust buckets after increasing number of buckets available

--- include/hash_bucket.h
+++ include/hash_bucket.h
@@ -1,14 +1,16 @@
 #ifndef __HASH_BUCKET_H__
 #define __HASH_BUCKET_H__ 1
 
-//#define SET_LOAD_FACTOR(bo, l) \
-//	float __lf = ((l) < (float)1.0 && (l) > (float)0.4 ? (l) : (float)0.75)
+#include <stdint.h>
+#include <sys/types.h>
 
 #define LOAD_FACTOR(bo) \
 (float)((float)(bo)->nr_buckets_used / (float)(bo)->nr_buckets)
 
 typedef struct Bucket
 {
+	char *key;
+	uint32_t hash; // the hash of the key
 	void *data;
 	size_t data_len;
 	int used;
@@ -24,7 +26,8 @@ typedef struct Bucket_Object
 } bucket_obj_t;
 
 bucket_obj_t *BUCKET_object_new(void);
-void BUCKET_object_destroy(bucket_obj_t *);
-void BUCKET_put_data(bucket_obj_t *, char *, char *);
+void BUCKET_object_destroy(bucket_obj_t *bObj);
+void BUCKET_put_data(bucket_obj_t *bObj, char *key, char *data);
+bucket_t *BUCKET_get_bucket(bucket_obj_t *bObj, char *key);
 
 #endif /* !defined __HASH_BUCKET_H__ */
