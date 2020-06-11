a8042be5c82ff3874372445cc04b0588fbc3dd3d
gamh86 <g.amh.86@outlook.fr>
2020-06-11 15:24:56 +0100

    Add/adjust struct definitions

--- include/xml.h
+++ include/xml.h
@@ -1,31 +1,39 @@
 #ifndef __XML_h__
 #define __XML_h__ 1
 
+/*
+ * For saving attributes of a tag, such as
+ * <tagname attribute1="value1" attribute2="value2">
+ */
+typedef struct Attribute
+{
+	char *name;
+	char *value;
+} attribute_t;
+
 typedef struct XML_Node
 {
-	char *n_value;
-	int n_type;
-	struct XML_Node **n_children; // array of node pointers
-	int n_nr_children;
+	char *name;
+	char *value;
+	attribute_t *attributes;
+	struct XML_Node **children; // array of node pointers
+	int nr_children;
+	int nr_attributes;
 } xml_node_t;
+
+struct XML
+{
+	xml_node_t *root;
+	int nr_nodes;
+};
 
 typedef xml_node_t *node_ptr;
 
-typedef struct XML_Tree
-{
-	xml_node_t *x_root;
-	int x_nr_nodes;
-} xml_tree_t;
-
-#define XML_NODE_VALUE(n) ((n)->n_value)
-
-/*
- * Parse XML data in file specified by PATH.
- * Return an XML tree object.
- */
-xml_tree_t *parse_xml_file(char *path);
-xml_node_t *XML_find_node(xml_tree_t *, char *);
-char *XML_node_get_value(xml_node_t *);
-void free_xml_tree(xml_tree_t *);
+struct XML *XML_new(void);
+int XML_parse_file(struct XML *, char *);
+xml_node_t *XML_find_by_path(struct XML *, char *);
+xml_node_t *XML_find_parent_node_for_value(xml_node_t *, char *);
+char *XML_get_node_value(xml_node_t *, char *);
+void XML_free(struct XML *);
 
 #endif /* !defined __XML_h__ */
